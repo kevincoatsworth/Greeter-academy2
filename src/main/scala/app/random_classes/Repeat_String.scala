ac0ad765076fbacac8bb0c85fc645607e5f7b66c
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-01-30 10:40:51 +0000

    amend println to print on repeat string.

--- src/main/scala/app/random_classes/Repeat_String.scala
+++ src/main/scala/app/random_classes/Repeat_String.scala
@@ -4,13 +4,13 @@ import scala.io.StdIn
 
 object Repeat_String extends App {
 
-  var userString = StdIn.readLine("Please enter a String: ")
+  val userString = StdIn.readLine("Please enter a String: ")
   println("Please enter an Int: ")
-  var userInt = StdIn.readInt()
+  val userInt = StdIn.readInt()
 
   for(x <-1 to userInt){
 
-    println(userString)
+    print(userString)
 
   }
 
