0e947316fa64af632b0854cf0515d7f37acc5e79
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-02-15 12:53:24 +0000

    Add Exception Handling to age of new person.

--- src/main/scala/app/controllers/GreeterApplication2.scala
+++ src/main/scala/app/controllers/GreeterApplication2.scala
@@ -27,7 +27,7 @@ object GreeterApplication2 extends App {
   val superAccount = new CashISASavingsAccount("1234566", 0.00, depositThreshold = 1000.00).deposit(1000.00).withdraw(300.00)
   val superPersonWithISA = new Person("Super Loyal customer", 56, List(superAccount))
 
-  println(superPersonWithISA.speak())
+  //println(superPersonWithISA.speak())
   /*
   val normalAccount = new CashISASavingsAccount("12334", 100.00)
   val loyalAccountDeposited = normalAccount.deposit(300.00)
@@ -35,15 +35,51 @@ object GreeterApplication2 extends App {
   println(loyal.speak())
   */
 
-  val name = Prompt.ask("What is your name? ")
-  val age = Prompt.ask("What is your age? ")
-  val p = new Person(name, age.toInt,Nil)
+  def prompt() : (String, Int) = {
+
+    val name : String = Prompt.ask("What is your name? ")
+
+    def getAge() : Int = {
+
+      try{
+
+        val age : Int = Prompt.ask("What is your age? ").toInt
+        age
+        } catch {
+
+        case _: NumberFormatException => getAge()
+        }
+
+    }
+
+    (name, getAge())
+
+  }
+
+  val (name, age) = prompt()
+  val p = new Person(name, age, Nil)
+
+/*
+  def CreateNewPerson {
+
+    val name = Prompt.ask("What is your name? ")
+    val age = Prompt.ask("What is your age? ")
+
+    try {
+      val p = new Person(name, age.toInt, Nil)
+    } catch {
+      case ex: NumberFormatException => {
+        CreateNewPerson
+      }
+    }
+  }
+*/
 
   val child = new Person("David")
   val p2 = new Person("Adam", "Conder")
 
   //println(p2.speak())
   //println(child.speak())
-  println(p.speak())
+  //println(p.speak())
 
 }
